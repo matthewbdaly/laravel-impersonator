1f4080d9281bcccf5ef3ac99037170b6ac6da285
Matthew Daly <matthewbdaly@gmail.com>
2017-11-17 19:33:05 +0000

    Added base test case

---
+++ tests/TestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+	protected function getPackageProviders($app)
+	{
+		return ['Matthewbdaly\LaravelImpersonator\Providers\ImpersonatorServiceProvider'];
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->artisan('migrate', ['--database' => 'sqlite']);
+        $this->loadLaravelMigrations(['--database' => 'sqlite']);
+        $this->withFactories(__DIR__.'/factories');
+    }
+}
