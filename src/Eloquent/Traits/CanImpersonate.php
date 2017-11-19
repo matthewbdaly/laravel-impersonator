af7bcfc6ca4796845a89fc7acf466bab6cd3c740
Matthew Daly <matthewbdaly@gmail.com>
2017-11-19 13:23:20 +0000

    Commented the trait

--- src/Eloquent/Traits/CanImpersonate.php
+++ src/Eloquent/Traits/CanImpersonate.php
@@ -4,18 +4,41 @@ namespace Matthewbdaly\LaravelImpersonator\Eloquent\Traits;
 
 use Illuminate\Contracts\Session\Session;
 
+/**
+ * Add methods to user model to enable them to impersonate users
+ *
+ */
 trait CanImpersonate
 {
+    /**
+     * Start impersonating user by ID
+     *
+     * @param integer $id      The user ID to impersonate.
+     * @param Session $session The session instance.
+     * @return void
+     */
     public function startImpersonating(int $id, Session $session)
     {
         $session->put('impersonate', $id);
     }
 
+    /**
+     * Stop impersonating user
+     *
+     * @param Session $session The session instance.
+     * @return void
+     */
     public function stopImpersonating(Session $session)
     {
         $session->forget('impersonate');
     }
 
+    /**
+     * Is user currently impersonating another user?
+     *
+     * @param Session $session The session instance.
+     * @return boolean
+     */
     public function isImpersonating(Session $session)
     {
         return $session->has('impersonate');
