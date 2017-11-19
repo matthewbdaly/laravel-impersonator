1fb5fa84f43de66cea202714ba0ec36a3d67d4b2
Matthew Daly <matthewbdaly@gmail.com>
2017-11-19 12:12:18 +0000

    Added basic failing test for the middleware

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,16 +6,4 @@ use Orchestra\Testbench\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
-	protected function getPackageProviders($app)
-	{
-		return ['Matthewbdaly\LaravelImpersonator\Providers\ImpersonatorServiceProvider'];
-    }
-
-    public function setUp()
-    {
-        parent::setUp();
-        $this->artisan('migrate', ['--database' => 'sqlite']);
-        $this->loadLaravelMigrations(['--database' => 'sqlite']);
-        $this->withFactories(__DIR__.'/factories');
-    }
 }
--- tests/Unit/Http/Middleware/ImpersonatorMiddlewareTest.php
+++ tests/Unit/Http/Middleware/ImpersonatorMiddlewareTest.php
@@ -4,7 +4,18 @@ namespace Tests\Unit\Http\Middleware;
 
 use Tests\TestCase;
 use Matthew\LaravelImpersonator\Http\Middleware\Impersonator;
+use Illuminate\Http\Request;
+use Mockery as m;
 
 class ImpersonatorMiddlewareTest extends TestCase
 {
+    public function testChangesUserIdIfNotSet()
+    {
+        $request = Request::create('http://example.com/admin', 'GET');
+        $response = m::mock('Illuminate\Http\Response');
+        $middleware = new Impersonator;
+        $middlewareResponse = $middleware->handle($request, function () use ($response) {
+            return $response;
+        });
+    }
 }
