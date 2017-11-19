1bc1a48e346657b95db2cb0df572dc6764715cae
Matthew Daly <matthewbdaly@gmail.com>
2017-11-19 12:00:03 +0000

    Added outline of middleware test

---
+++ tests/Unit/Http/Middleware/ImpersonatorMiddlewareTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tests\Unit\Http\Middleware;
+
+use Tests\TestCase;
+use Matthew\LaravelImpersonator\Http\Middleware\Impersonator;
+
+class ImpersonatorMiddlewareTest extends TestCase
+{
+}
