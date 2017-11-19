34dab5287dbe90768b00b5acf48491dda90b606f
Matthew Daly <matthewbdaly@gmail.com>
2017-11-19 12:47:38 +0000

    Copied over user factory and added DB config for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,4 +9,12 @@ use Mockery as m;
 class TestCase extends BaseTestCase
 {
     use MockeryPHPUnitIntegration;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->artisan('migrate', ['--database' => 'sqlite']);
+        $this->loadLaravelMigrations(['--database' => 'sqlite']);
+        $this->withFactories(__DIR__.'/factories');
+    }
 }
