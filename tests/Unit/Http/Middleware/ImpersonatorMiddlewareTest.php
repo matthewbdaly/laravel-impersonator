e4b65c44d7641b4c53c14f95beb2c5ddb1ee2e98
Matthew Daly <matthewbdaly@gmail.com>
2017-11-19 12:25:33 +0000

    Added tests for impersonator field being set and not set

--- tests/Unit/Http/Middleware/ImpersonatorMiddlewareTest.php
+++ tests/Unit/Http/Middleware/ImpersonatorMiddlewareTest.php
@@ -9,11 +9,29 @@ use Mockery as m;
 
 class ImpersonatorMiddlewareTest extends TestCase
 {
-    public function testChangesUserIdIfNotSet()
+    public function testChangesUserIdIfImpersonating()
     {
         $request = Request::create('http://example.com/admin', 'GET');
         $response = m::mock('Illuminate\Http\Response');
-        $middleware = new Impersonator;
+        $session = m::mock('Illuminate\Contracts\Session\Session');
+        $session->shouldReceive('has')->with('impersonate')->once()->andReturn(true);
+        $session->shouldReceive('get')->with('impersonate')->once()->andReturn(2);
+        $auth = m::mock('Illuminate\Contracts\Auth\Guard');
+        $auth->shouldReceive('onceUsingId')->with(2)->once();
+        $middleware = new Impersonator($auth, $session);
+        $middlewareResponse = $middleware->handle($request, function () use ($response) {
+            return $response;
+        });
+    }
+
+    public function testDoesNotChangeUserIdIfNotImpersonating()
+    {
+        $request = Request::create('http://example.com/admin', 'GET');
+        $response = m::mock('Illuminate\Http\Response');
+        $session = m::mock('Illuminate\Contracts\Session\Session');
+        $session->shouldReceive('has')->with('impersonate')->once()->andReturn(false);
+        $auth = m::mock('Illuminate\Contracts\Auth\Guard');
+        $middleware = new Impersonator($auth, $session);
         $middlewareResponse = $middleware->handle($request, function () use ($response) {
             return $response;
         });
