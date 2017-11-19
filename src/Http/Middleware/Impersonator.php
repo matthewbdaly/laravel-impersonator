a089033faac2dff097b2108fa3e2270f8a718306
Matthew Daly <matthewbdaly@gmail.com>
2017-11-19 12:41:47 +0000

    Tidied up the code base

--- src/Http/Middleware/Impersonator.php
+++ src/Http/Middleware/Impersonator.php
@@ -3,15 +3,36 @@
 namespace Matthewbdaly\LaravelImpersonator\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Session\Session;
 
+/**
+ * Impersonate another user
+ */
 class Impersonator
 {
+    /**
+     * Auth service
+     *
+     * @var $auth
+     */
     protected $auth;
 
+    /**
+     * Session service
+     *
+     * @var $session
+     */
     protected $session;
 
+    /**
+     * Constructor
+     *
+     * @param Guard   $auth    The auth instance.
+     * @param Session $session The session instance.
+     * @return void
+     */
     public function __construct(Guard $auth, Session $session)
     {
         $this->auth = $auth;
@@ -21,14 +42,13 @@ class Impersonator
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request The HTTP request object.
+     * @param  \Closure                 $next    The closure to return the response.
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        if($this->session->has('impersonate'))
-        {
+        if ($this->session->has('impersonate')) {
             $this->auth->onceUsingId($this->session->get('impersonate'));
         }
 
